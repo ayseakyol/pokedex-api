25279aed2c89bf196a5d0031fd8bf52d6a383030
ayseakyol <ayseakyol.22309@gmail.com>
2020-05-28 10:19:36 +0200

    logic

--- logic/type-stats/index.js
+++ logic/type-stats/index.js
@@ -1,5 +1,19 @@
 const typeStats = (pokeArray, type) => {
-
+  let typeCount = 0;
+  let weaknessCount = 0;
+  pokeArray.map((p) => {
+    if (p.type.includes(type)) {
+      typeCount++;
+    }
+    if (p.weaknesses.includes(type)) {
+      weaknessCount++;
+    }
+  });
+  return {
+    typeName: type,
+    typeCount: typeCount,
+    weaknessCount: weaknessCount,
+  };
 };
 
 module.exports = typeStats;
