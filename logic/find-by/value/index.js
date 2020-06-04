44e0d08256fa7743ddacf3ee499097add2872369
Ayse Akyol <59367104+ayseakyol@users.noreply.github.com>
2020-06-04 12:52:37 +0200

    Update index.js

--- logic/find-by/value/index.js
+++ logic/find-by/value/index.js
@@ -1,7 +1,6 @@
 // "value" will always be a string, because params always are!
 // consider using loose equality for your comparison
 // (later you will learn about req.body to pass any type)
-const isEqual = require("lodash").isEqual
 
 const value = (pokeArray, value) => {
   const arr = [];
